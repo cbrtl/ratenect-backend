cec31f7139e1c6dce173ea1e8e83aaa256e6d394
Ankur Khandelwal <khandelwalankur176@gmail.com>
2021-07-01 16:37:03 +0530

    set up the .env file

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
+
+const db_user = process.env.db_user;
+const db_pwd = process.env.db_pwd;
 
 app.set('view engine','ejs');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-mongoose.connect("mongodb+srv://ratenect:ratenect123@cluster0.57zvw.mongodb.net/ratenectDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://${db_user}:${db_pwd}@cluster0.57zvw.mongodb.net/ratenectDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.listen(3000,function(){
     console.log('Server started at port 3000');

1af50f5c90ad51465c64df3ab5a0928ab97cc778
Ankur Khandelwal <khandelwalankur176@gmail.com>
2021-11-02 19:08:18 +0530

    added new routes for ngo

--- routes/ngo.js
+++ routes/ngo.js
@@ -7,11 +7,16 @@ const {
 	ngologin,
 	createCampaign,
 	searchNgos,
+	saveNgoProfile,
+	getNgoData
 } = require('../controllers/ngo');
+// const {getUserData} = require('../common-middleware/getUserData');
 
 router.post('/ngosignup', ngosignup);
 router.post('/ngologin', ngologin);
 router.post('/createCampaign', createCampaign);
+router.post('/saveNgoProfile/:ngoId', saveNgoProfile); 
 router.get('/searchngos', searchNgos);
+router.get('/getNgoData', getNgoData);
 
 module.exports = router;
